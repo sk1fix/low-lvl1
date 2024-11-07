d334d29bc8189b85e16a72ca2b56fbfeed3a8b64
sk1fix <agafonovtoha427@gmail.com>
2024-11-07 11:46:12 +0400

    j

--- lab1/main2.cpp
+++ lab1/main2.cpp
@@ -5,18 +5,15 @@ int main1() {
     char str[256];
     int len, i;
 
-    // Ввод строки
     printf("Enter a string (max 255 characters): ");
     fgets(str, 256, stdin);
 
-    // Удаление символа новой строки
     len = strlen(str);
     if (str[len - 1] == '\n') {
         str[len - 1] = '\0';
         len--;
     }
 
-    // Вывод инвертированной строки
     printf("Inverted string: ");
     i = len - 1;
 inverted_loop:
@@ -27,7 +24,6 @@ inverted_loop:
 end_inverted:
     printf("\n");
 
-    // Вывод строки из четных символов
     printf("String of even elements: ");
     i = 1;
 even_loop:
